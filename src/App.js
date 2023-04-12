902b09b14f6d214abf0a22e206bc90968699208a
lcc-miura <miura_asahi@lightcafe.co.jp>
2023-04-12 13:01:07 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginButton from './login';
+import Profile from './profile';
+import LogoutButton from './logout';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <LoginButton></LoginButton>
+        <LogoutButton></LogoutButton>
+        <Profile></Profile>
+      </header>
+    </div>
+  );
+}
+
+export default App;
